17f030336eab8a4de8ce442167dcc6e9b501a0ec
kievmastadjedai@gmail.com <kievmastadjedai@gmail.com>
2023-09-22 15:35:50 +0300

    Добавьте файлы проекта.

---
+++ AutoMarketBot/MainConfig.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.ReplyMarkups;
+
+namespace AutoMarketBot
+{
+    public class MainConfig
+    {
+        
+        async private Task MainKeyboard(ITelegramBotClient botClient,Message message)
+        {
+            ReplyKeyboardMarkup keyboardMarkup = new(new[]
+            {
+                new KeyboardButton[] { "Оформити замовлення" },
+                new KeyboardButton[] { "Переглянути товар" }
+            })
+            {
+                ResizeKeyboard = true
+            };
+            Message sendMainKeyboard = await botClient.SendTextMessageAsync(message.Chat.Id, "Чим допомогти?", replyMarkup:keyboardMarkup);
+        }
+        async private Task CheckoutKeyboard(ITelegramBotClient botClient, Message message)
+        {
+            ReplyKeyboardMarkup keyboardMarkup = new(new[]
+            {
+                new KeyboardButton[]{"Кошик"},
+                new KeyboardButton[]{"Замовити"},
+                new KeyboardButton[]{"Назад"}
+            })
+            {
+                ResizeKeyboard = true
+            };
+            Message sendChekoutKeyboard = await botClient.SendTextMessageAsync(message.Chat.Id,"(*_*)",replyMarkup:keyboardMarkup);
+        }
+
+        async private Task BasketInline(ITelegramBotClient botClient, Message message)
+        {
+            InlineKeyboardMarkup inlineKeyboard = new(new[]
+            {
+                new[]
+                {
+                    InlineKeyboardButton.WithSwitchInlineQueryCurrentChat(text:"querry","quer")
+                }
+            });
+            
+            Message sendMessage = await botClient.SendTextMessageAsync(message.Chat.Id, "Каталог", replyMarkup: inlineKeyboard);
+
+        }
+        async public Task HandleMessage(ITelegramBotClient botClient,Message message)
+        {
+            if(message.Text == "/start")
+            {
+                await MainKeyboard(botClient, message);
+            }
+            if(message.Text == "Назад")
+            {
+                await MainKeyboard(botClient, message);
+            }
+            if(message.Text == "Оформити замовлення")
+            {
+                await CheckoutKeyboard(botClient, message);
+            }
+            if(message.Text == "Переглянути товар")
+            {
+                await BasketInline(botClient, message);
+            }
+        }
+
+    }
+}
---
+++ AutoMarketBot/Program.cs
@@ -0,0 +1,60 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Exceptions;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+
+namespace AutoMarketBot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var botClient = new TelegramBotClient("6587747565:AAHD7bZ_Mgi-HVAPRu6akH80C3HKPMaeiNo");
+
+            using CancellationTokenSource cts = new CancellationTokenSource();
+
+            ReceiverOptions receiverOptions = new ReceiverOptions()
+            {
+                AllowedUpdates = Array.Empty<UpdateType>()
+            };
+
+            botClient.StartReceiving(HandleUpdateAsync, HandlePollingErrorAsync, receiverOptions, cancellationToken: cts.Token);
+
+            Console.ReadLine();
+
+
+            async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken token)
+            {
+                MainConfig config = new MainConfig();
+                if(update.Type == UpdateType.Message && update?.Message?.Text != null)
+                {
+                    try
+                    {
+                        await config.HandleMessage(botClient, update.Message);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                }
+            }
+
+        }
+
+
+
+        private static Task HandlePollingErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken token)
+        {
+            var ErrorMessage = exception switch
+            {
+                ApiRequestException apiRequestException
+                => $"Telegram API Error:\n[{apiRequestException.ErrorCode}]\n{apiRequestException.Message}",
+                _ => exception.ToString()
+            };
+            Console.WriteLine(ErrorMessage);
+            return Task.CompletedTask;
+        }
+        
+    }
+}
